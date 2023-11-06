ce2fc75c31d815a70663517172a84d4a2e83b484
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-06 19:55:29 +0100

    feature: update env variable with to match Vercel name

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,20 @@
-export { GET, POST } from '@/auth'
+export { GET, POST } from '@/auth';
 export const runtime = 'edge'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    // ...add any other providers you want to use
+  ],
+  // ...add other NextAuth options here as needed
+});
