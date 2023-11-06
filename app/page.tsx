9677a7d662b39ceeda31b699981740a874c767d8
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-07 00:15:17 +0100

    feat: add use client to app/age.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
-// app/page.tsx
+// Add this directive to indicate that this component should be treated as a Client Component
+"use client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { nanoid } from '@/lib/utils';
 
+export const runtime = 'edge'; // Keep this if you are running at the Edge runtime
+
 export default function IndexPage() {
-  // Generate a new unique chat ID
-  const id = nanoid();
-  
-  // Perform client-side redirection using window.location.href
-  if (typeof window !== 'undefined') {
-    window.location.href = `/chat/${id}`;
-  }
+  const router = useRouter();
+
+  useEffect(() => {
+    // Generate a new unique chat ID
+    const id = nanoid();
+    // Redirect to the chat page with the generated ID
+    router.push(`/chat/${id}`);
+  }, [router]);
 
   // You can render a loading indicator here
   return <div>Loading...</div>;
 }
+
