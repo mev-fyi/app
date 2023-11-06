120b354895902158e8e914fc05fe5e487b997873
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-07 00:01:17 +0100

    fix: page.tsx using client

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
 // app/page.tsx
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { nanoid } from '@/lib/utils';
 
-export const runtime = 'edge';
-
 export default function IndexPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Generate a new unique chat ID
-    const id = nanoid();
-    // Redirect to the chat page with the generated ID
-    router.push(`/chat/${id}`);
-  }, [router]);
+  // Generate a new unique chat ID
+  const id = nanoid();
+  
+  // Perform client-side redirection using window.location.href
+  if (typeof window !== 'undefined') {
+    window.location.href = `/chat/${id}`;
+  }
 
   // You can render a loading indicator here
   return <div>Loading...</div>;
