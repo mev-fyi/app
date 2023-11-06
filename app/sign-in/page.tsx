6c7cb7ab1d463b394ee3531991b9aca88d1ff4a8
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-06 18:23:10 +0100

    features: comment out authentification for now

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,14 +1,17 @@
 import { auth } from '@/auth'
 import { GithubLoginButton } from '@/components/login-button'
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation' // Commented out as we're not using it now
 import { LandingPage } from '@/components/landing-page'
+
 export default async function SignInPage() {
-  const session = await auth()
-  console.log('Sign-in session', session)
-  // redirect to home if user is already logged in
-  if (session?.user) {
-    redirect('/')
-  }
+  // const session = await auth() // Commented out as we're not using session to redirect
+  // console.log('Sign-in session', session)
+  
+  // Redirect to home if user is already logged in (currently not in use)
+  // if (session?.user) {
+  //   redirect('/')
+  // }
+  
   return (
     <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
       <LandingPage />
