bcb1a6a06a8e1d0e48ab46caf00b317578d2146f
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-08 18:07:17 +0100

    feat: add logging in sign-in/page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,10 +4,16 @@ import { redirect } from 'next/navigation'
 
 export default async function SignInPage() {
   const session = await auth()
-  // redirect to home if user is already logged in
+
+  // Log the result of the authentication check
   if (session?.user) {
+    console.log(`User ${session.user.id} is already logged in. Redirecting to home page.`)
     redirect('/')
+    return null // Prevent further rendering since we are redirecting
+  } else {
+    console.log('Rendering sign-in page for anonymous user.')
   }
+
   return (
     <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
       <LoginButton />
