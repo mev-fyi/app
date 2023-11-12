59cd96e5bb3a2cdc9c666b3176dbbf59c659b008
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-12 20:45:08 +0100

    feat: try adding chat with extended message

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useChat, UseChatOptions, type Message } from 'ai/react';
+import { type Message } from 'ai/react';
 import { cn } from '@/lib/utils'
 import { ChatList } from '@/components/chat-list'
 import { ChatPanel } from '@/components/chat-panel'
@@ -19,14 +19,23 @@ import { useState } from 'react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { toast } from 'react-hot-toast'
+import { useExtendedChat } from 'lib/hooks/use-extended-chat';
+import MetadataList from '@/components/metadata-list';
+
+
+// Extend the Message type to include structured_metadata
+interface MetadataMessage extends Message {
+  structured_metadata?: any[]; // Ideally, define a more specific type instead of any[]
+}
 
 const IS_PREVIEW = process.env.VERCEL_ENV === 'preview'
 export interface ChatProps extends React.ComponentProps<'div'> {
-  initialMessages?: Message[]
+  initialMessages?: MetadataMessage[]
   id?: string
 }
 
 export function Chat({ id, initialMessages, className }: ChatProps) {
+  console.log('Chat component rendering with id:', id);
   const [previewToken, setPreviewToken] = useLocalStorage<string | null>(
     'ai-token',
     null
@@ -34,7 +43,7 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
   const [previewTokenDialog, setPreviewTokenDialog] = useState(IS_PREVIEW)
   const [previewTokenInput, setPreviewTokenInput] = useState(previewToken ?? '')
   const { messages, append, reload, stop, isLoading, input, setInput } =
-  useChat({
+  useExtendedChat({
       initialMessages,
       id,
       body: {
@@ -48,6 +57,15 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
       }
     })
   
+    console.log('Messages state:', messages);
+
+    // Extract all metadata entries into a single array
+   const metadataEntries = messages.flatMap(msg => 
+     msg.role === 'assistant' && msg.structured_metadata ? msg.structured_metadata : []
+   );
+
+   console.log('Metadata entries:', metadataEntries);
+
   return (
     <>
       <div className={cn('pb-[200px] pt-4 md:pt-10', className)}>
@@ -70,6 +88,10 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
         input={input}
         setInput={setInput}
       />
+      <div className="w-full md:w-80 p-4 overflow-auto">
+        {/* Metadata section */}
+        <MetadataList entries={metadataEntries} />
+      </div>
       <Dialog open={previewTokenDialog} onOpenChange={setPreviewTokenDialog}>
         <DialogContent>
           <DialogHeader>
