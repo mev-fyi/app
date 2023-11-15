6ca1754bc97b0652388278dc41c5dd8c662a5833
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-15 19:15:48 +0100

    fix attempt at react-server-dom-webpack by changing pages/chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,37 +1,45 @@
+import { useEffect, useState } from 'react';
 import { GetServerSideProps } from 'next';
+import dynamic from 'next/dynamic';
 import { type Chat } from '@/lib/types';
 import { manageSessionID } from '@/lib/utils';
 import { getChat } from '@/app/actions';
-import { Chat as ChatComponent } from '@/components/chat'
+
+// Dynamically import the Chat component with ssr set to false
+const ChatComponent = dynamic(() => import('@/components/chat'), { ssr: false });
 
 interface ChatPageProps {
   chatData: Chat;
 }
 
 function ChatPage({ chatData }: ChatPageProps) {
+  const [isClientSide, setIsClientSide] = useState(false);
+
+  useEffect(() => {
+    // Set state to true when component mounts in the browser
+    setIsClientSide(typeof window !== 'undefined');
+  }, []);
+
   if (!chatData) {
     return <div>Chat not found.</div>;
   }
 
   return (
     <div>
-      {/* Render the chat data or a loading state here */}
-      {/* Client-side specific code will go here */}
-      {typeof window !== 'undefined' && (
-        <ChatComponent id={chatData.id} initialMessages={chatData.messages} />
-      )}
+      {/* Client-side specific code will render here */}
+      {isClientSide && <ChatComponent id={chatData.id} initialMessages={chatData.messages} />}
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const sessionId = manageSessionID(context.req, context.res);
-  
-  // Your logic to get chat data...
   const { id } = context.params || {};
+  
   if (typeof id !== 'string') {
     return { notFound: true };
   }
+
   const chatData = await getChat(id, sessionId);
   return { props: { chatData } };
 };
--- reinstall_dependencies.sh
+++ reinstall_dependencies.sh
@@ -1,9 +1,10 @@
+clear
 rm -rf node_modules/
 rm -f package-lock.json
 rm -f  yarn.lock
 rm -f  pnpm-lock.yaml
 rm -rf .next/
 npm cache clean --force
-npm install
+# npm install
 npm install --legacy-peer-deps
 npm run build
