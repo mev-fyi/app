3a99628a603644c4cbc97fdbfb1fb1ed5e8e7364
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-15 20:51:42 +0100

    fix attempt: add more logging around loading the chat component dynamically in pages/chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -5,35 +5,49 @@ import { type Chat } from '@/lib/types';
 const ChatPage = () => {
   const [ChatComponent, setChatComponent] = useState<React.ComponentType<{ id: string; initialMessages: any[] }> | null>(null);
   const [chatData, setChatData] = useState<Chat | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    // Fetch chat data based on session ID (you might need to adjust the API call)
+    console.log('Fetching chat data...');
+
     fetch('/api/chat')
-      .then((res) => res.json())
+      .then((res) => {
+        console.log('Response from /api/chat:', res);
+        return res.json();
+      })
       .then((data) => {
+        console.log('Chat data:', data);
         setChatData(data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching chat data:', error);
+        setIsLoading(false);
         // Handle error, e.g., redirect to an error page or show a message
       });
 
     if (typeof window !== 'undefined') {
+      console.log('Importing ChatComponent...');
       import('@/components/chat').then((mod) => {
+        console.log('ChatComponent loaded:', mod.Chat);
         setChatComponent(() => mod.Chat);
-      });
+      }).catch(error => console.error('Error importing ChatComponent:', error));
+    } else {
+      console.log('Window object not found, skipping ChatComponent import.');
     }
   }, []);
 
-  if (!chatData) {
+  if (isLoading) {
     return <p>Loading chat...</p>;
   }
 
+  if (!chatData || !ChatComponent) {
+    return <p>Chat not found or failed to load component.</p>;
+  }
+
   return (
-    <div>
-      {ChatComponent ? <ChatComponent id={chatData.id} initialMessages={chatData.messages} /> : <p>Loading chat...</p>}
-    </div>
+    <ChatComponent id={chatData.id} initialMessages={chatData.messages} />
   );
 };
 
