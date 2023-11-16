52d21c324994e4af5163ff55f012391b7384c173
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-16 18:30:46 +0100

    feat: make both empty-screen questions and chatlist 15% smaller

--- components/questions-list.tsx
+++ components/questions-list.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { IconArrowRight } from '@/components/ui/icons';
 import { questions } from '@/lib/constants'; // Ensure the path is correct
+import styles from './ChatListContainer.module.css'; // Adjust the path if necessary
 
 export const QuestionList = ({ setInput }: Pick<UseChatHelpers, 'setInput'>) => {
   const [selectedQuestions, setSelectedQuestions] = useState<string[]>([]);
@@ -20,14 +21,14 @@ export const QuestionList = ({ setInput }: Pick<UseChatHelpers, 'setInput'>) => 
     <div>
       {selectedQuestions.map((question, index) => (
         <div key={index} className="mb-4"> {/* Add margin-bottom to each question */}
-          <Button
-            variant="link"
-            className="text-left text-base text-white flex items-center w-full"
-            onClick={() => setInput(question)}
-          >
-            <IconArrowRight className="flex-shrink-0 mr-2" />
-            <span className="break-words flex-grow">{question}</span>
-          </Button>
+        <Button
+          variant="link"
+          className="text-left text-base text-white flex items-center w-full"
+          onClick={() => setInput(question)}
+        >
+          <IconArrowRight className="flex-shrink-0 mr-2" />
+          <span className={`break-words flex-grow ${styles.customMarkdownFont}`}>{question}</span>
+        </Button>
         </div>
       ))}
       {/* New Button to shuffle and select four questions */}
