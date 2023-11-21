3563c2d94b3897ed46bfd9c61b75d8e00ebc9745
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-22 00:01:12 +0100

    feat: add tons of default questions to spoil my users

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,14 +12,14 @@ const configuration = new Configuration({
 });
 
 export async function POST(req: Request) {
-  console.log('Received POST request');
+  console.log('route.ts: Received POST request');
 
   let json;
   try {
     json = await req.json();
-    console.log('JSON body parsed:', json);
+    console.log('route.ts: JSON body parsed:', json);
   } catch (error) {
-    console.error('Error parsing JSON body:', error);
+    console.error('route.ts: Error parsing JSON body:', error);
     return new Response('Bad request', { status: 400 });
   }
 
@@ -27,50 +27,50 @@ export async function POST(req: Request) {
   const session = await auth();
 
   if (!session?.user) {
-    console.error('Unauthorized request: No session user found.');
+    console.error('route.ts: Unauthorized request: No session user found.');
     return new Response('Unauthorized', { status: 401 });
   }
 
-  console.log('User authenticated, user ID:', session.user.id);
+  console.log('route.ts: User authenticated, user ID:', session.user.id);
 
   if (previewToken) {
-    console.log('Using preview token for API key');
+    console.log('route.ts: Using preview token for API key');
     configuration.apiKey = previewToken;
   }
 
   const mostRecentMessageContent = messages.length > 0 ? messages[messages.length - 1].content : "No messages yet.";
-  console.log('All messages:', messages);
-  console.log('Most recent message content:', mostRecentMessageContent);
+  console.log('route.ts: All messages:', messages);
+  console.log('route.ts: Most recent message content:', mostRecentMessageContent);
 
   const backendChatUrl = `${process.env.REACT_APP_BACKEND_URL}/chat`;
-  console.log('Backend chat URL:', backendChatUrl);
+  console.log('route.ts: Backend chat URL:', backendChatUrl);
 
   let chatResponse;
   try {
-    console.log('Attempting to send request to backend: ', mostRecentMessageContent)
+    console.log('route.ts: Attempting to send request to backend: ', mostRecentMessageContent)
     chatResponse = await fetch(backendChatUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: mostRecentMessageContent, chat_history: messages })
     });
-    console.log('Chat response received', chatResponse);
+    console.log('route.ts: Chat response received', chatResponse);
   } catch (error) {
-    console.error('Fetch to backend chat failed:', error);
+    console.error('route.ts: Fetch to backend chat failed:', error);
     return new Response('Internal Server Error', { status: 500 });
   }
 
   if (!chatResponse.ok) {
-    console.error(`Backend failed to process chat message with status ${chatResponse.status}`);
+    console.error(`route.ts: Backend failed to process chat message with status ${chatResponse.status}`);
     return new Response(`Error from backend service: ${chatResponse.statusText}`, { status: chatResponse.status });
   }
 
   const responseBody = await chatResponse.json();
-  console.log(`Received response from backend:`, responseBody);
+  console.log(`route.ts: Received response from backend:`, responseBody);
 
   let structuredMetadata: ParsedMetadataEntry[] = [];
   if (responseBody.formatted_metadata) {
     structuredMetadata = parseMetadata(responseBody.formatted_metadata);
-    console.log('Parsed metadata:', structuredMetadata);
+    console.log('route.ts: Parsed metadata:', structuredMetadata);
   }
 
   const title = messages[0]?.content.substring(0, 100) || "New Chat";
@@ -97,7 +97,7 @@ export async function POST(req: Request) {
   try {
     await kv.set(`chat:${id}`, JSON.stringify(payload));
     await kv.zadd(`user:chat:${session.user.id}`, { score: createdAt, member: `chat:${id}` });
-    console.log('Chat record stored');
+    console.log('route.ts: Chat record stored');
   } catch (error) {
     console.error('Failed to store chat record:', error);
     return new Response('Internal Server Error', { status: 500 });
