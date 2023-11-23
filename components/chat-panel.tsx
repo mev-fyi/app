6e55567b9d5a139a7600ff32d1cde136659642b3
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-23 02:37:57 +0100

    feat: remove gradient background color from chatpanel, border-t from prompt form

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -41,7 +41,7 @@ export function ChatPanel({
   setLastMessageRole
 }: ChatPanelProps) {
   return (
-    <div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50%">
+    <div className="fixed inset-x-0 bottom-0 ">
       <ButtonScrollToBottom />
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         <div className="flex h-10 items-center justify-center">
@@ -67,7 +67,7 @@ export function ChatPanel({
             )
           )}
         </div>
-        <div className="space-y-4 border-t px-4 py-2 sm:rounded-t-xl sm:border md:py-4">
+        <div className="space-y-4 px-4 py-2 sm:rounded-t-xl sm:border md:py-4">
           <PromptForm
             onSubmit={async value => {
               if (onSubmit) {
