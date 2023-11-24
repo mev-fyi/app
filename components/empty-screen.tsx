8a65eb238445f953c6d5bddcc25140c877ea8a99
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-24 18:43:10 +0100

    feat: reduce emptyscreen intro

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -28,11 +28,8 @@ export function EmptyScreen({setInput}: QuestionsOverlayProps) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8 text-left">
         <h1 className="mb-2 text-lg font-semibold text-white">
-          Welcome to the mev.fyi Chatbot!
-        </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
           mev.fyi is the Maximal Extractable Value (MEV) research chatbot.
-        </p>
+        </h1>
         <p className="mb-4 leading-normal text-muted-foreground">
           Find the latest MEV-related research, 
           across mechanism design, auctions, information privacy, from research papers and YouTube videos.
