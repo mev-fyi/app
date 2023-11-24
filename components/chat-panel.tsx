d36390c327bb10557c5ab99a310c2ecae7a8ad96
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-24 16:23:26 +0100

    feat: propagate to promptform in chatpanel

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -67,7 +67,7 @@ export function ChatPanel({
             )
           )}
         </div>
-        <div className="space-y-4 px-4 py-2 sm:rounded-t-xl md:py-4">
+        <div className="space-y-4 px-4 py-2 sm:rounded-t-xl md:py-4 bg-black sm:bg-transparent">
           <PromptForm
             onSubmit={async value => {
               if (onSubmit) {
