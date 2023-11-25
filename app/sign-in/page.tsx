804308f5d16e9d2b4c17c9dfd180c7282f85e04d
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-25 01:08:32 +0100

    feat: propagate to sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from '@/auth'
 import { LoginButton } from '@/components/login-button'
 import { redirect } from 'next/navigation'
+import Image from 'next/image';
 
 export default async function SignInPage() {
   const session = await auth()
@@ -9,9 +10,25 @@ export default async function SignInPage() {
     redirect('/')
   }
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <LoginButton loginType="github" text="Login with GitHub" showIcon />
-      <LoginButton loginType="google" text="Login with Google" showIcon />
+    <div className="flex flex-col h-screen items-center justify-center py-10">
+      {/* Welcome message */}
+      <h1 className="mb-4 text-3xl font-bold text-center">mev.fyi is the Maximal Extractable Value (MEV) research chatbot.</h1>
+      <p className="mb-4 leading-normal text-muted-foreground">
+          Find the latest MEV-related research, 
+          across mechanism design, auctions, information privacy, from research papers and YouTube videos.
+      </p>
+      
+      {/* Welcome image */}
+      <div className="mb-8">
+        {/* Since the image is in the public folder, you can refer to it directly by its path */}
+        <Image src="/android-chrome-512x512.png" alt="Welcome" width={256} height={256} />
+      </div>
+      
+      {/* Login buttons */}
+      <div>
+        <LoginButton loginType="github" text="Login with GitHub" showIcon />
+        <LoginButton loginType="google" text="Login with Google" showIcon />
+      </div>
     </div>
   );
 }
