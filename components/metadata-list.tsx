49c76dcfb41db8b59a99a905d9deb7d313b96846
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-27 15:45:27 +0100

    feat: add youtube thumbnails

--- components/metadata-list.tsx
+++ components/metadata-list.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import { ParsedMetadataEntry } from '@/lib/types'; // Adjust the import path as necessary
-import styles from './MetadataList.module.css'; // Import the CSS module
-import {IconResearchPaper, IconYouTube} from '@/components/ui/icons'; // Adjust path as necessary
+import { ParsedMetadataEntry } from '@/lib/types';
+import styles from './MetadataList.module.css';
 
-interface MetadataListProps {
-  entries: ParsedMetadataEntry[];
-}
-
-const MetadataList: React.FC<MetadataListProps> = ({ entries }) => {
-  // Function to determine if an entry is a YouTube video
-  const isYoutubeVideo = (entry: ParsedMetadataEntry) => entry.type === 'youtubeVideo';
+const MetadataList: React.FC<{ entries: ParsedMetadataEntry[] }> = ({ entries }) => {
+  // Function to get thumbnail URL
+  const getThumbnailUrl = (entry: ParsedMetadataEntry) => {
+    if (entry.type === 'youtubeVideo') {
+      const videoId = new URL(entry.link).searchParams.get('v');
+      return videoId ? `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` : '/default-youtube-thumbnail.jpg'; // Fallback thumbnail
+    } else if (entry.type === 'researchPaper') {
+      return '/default-research-paper-thumbnail.jpg'; // Default or specific thumbnail for research papers
+    }
+    return '/default-thumbnail.jpg'; // General fallback thumbnail
+  };
 
   return (
     <ol className={styles.metadataList}>
       {entries.map((entry, index) => (
         <li key={index} className={styles.metadataListItem}>
-          <div className={styles.metadataListItemIcon}>
-            {isYoutubeVideo(entry) ? <IconYouTube /> : <IconResearchPaper />}
+          <div className={styles.metadataThumbnail}>
+            <img src={getThumbnailUrl(entry)} alt={entry.title} />
           </div>
-          <a href={entry.link} target="_blank" rel="noopener noreferrer" className={styles.metadataListLink}>
-            {entry.title}
-          </a>
-          <span className={styles.metadataListSpan}>
-            {entry.extraInfo} &middot; {entry.publishedDateString}
-          </span>
+          <div className={styles.metadataContent}>
+            <a href={entry.link} target="_blank" rel="noopener noreferrer" className={styles.metadataListLink}>
+              {entry.title}
+            </a>
+            <span className={styles.metadataListSpan}>
+              {entry.extraInfo} &middot; {entry.publishedDateString}
+            </span>
+          </div>
         </li>
       ))}
     </ol>
