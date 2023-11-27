e6656bb39b250fe1dbd88cab9ba2688e5ea7ede2
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-27 16:35:43 +0100

    feat: try encoding title for matching

--- components/metadata-list.tsx
+++ components/metadata-list.tsx
@@ -3,19 +3,14 @@ import { ParsedMetadataEntry } from '@/lib/types';
 import styles from './MetadataList.module.css';
 
 const MetadataList: React.FC<{ entries: ParsedMetadataEntry[] }> = ({ entries }) => {
-  // Function to sanitize the title to match filename conventions
-  // const sanitizeTitle = (title: string) => {
-  //   return title.replace(/[^a-zA-Z0-9]/g, '_'); // Replace special characters with underscores
-  // };
-
   // Function to get thumbnail URL
   const getThumbnailUrl = (entry: ParsedMetadataEntry) => {
     if (entry.type === 'youtubeVideo') {
       const videoId = new URL(entry.link).searchParams.get('v');
       return videoId ? `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` : '/default-youtube-thumbnail.jpg'; // Fallback thumbnail
     } else if (entry.type === 'researchPaper') {
-      const sanitizedTitle = entry.title;
-      return `/assets/research_paper_thumbnails/${sanitizedTitle}.png`; // Path to the thumbnail in the assets directory
+      const encodedTitle = encodeURIComponent(entry.title) + '.png'; // Encode the title
+      return `/assets/research_paper_thumbnails/${encodedTitle}`; // Path to the thumbnail in the assets directory
     }
     return '/default-thumbnail.jpg'; // General fallback thumbnail
   };
