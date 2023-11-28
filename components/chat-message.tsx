389ce4fa03803820298c17b87ed81aaa515119f8
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-28 01:07:30 +0100

    feat: remove white background to iconopenai

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -43,10 +43,10 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
     >
       <div
         className={cn(
-          'flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow',
-          message.role === 'user'
-            ? 'bg-background'
-            : 'bg-primary text-primary-foreground'
+          'flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow bg-background'//,
+          // message.role === 'user'
+          //   ? 'bg-background'
+          //   : 'bg-primary text-primary-foreground'
         )}
       >
         
