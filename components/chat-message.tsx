2d38b9c5dc3c9ac1190a3c807de1a9c5f1c95e95
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-12-06 02:34:35 +0100

    feat: add explorer.png to see how it renders for user icon

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -36,6 +36,7 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
   // <IconOpenAI />
+  // {message.role === 'user' ? <IconUser /> : '🤖'}
   return (
     <div
       className={cn('group relative mb-4 flex items-start md:-ml-12')}
@@ -50,7 +51,9 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
         )}
       >
         
-        {message.role === 'user' ? <IconUser /> : '🤖'}
+        {message.role === 'user' ? 
+          <img src="/explorer.png" alt="User Icon" className="h-full w-full object-cover" /> 
+          : '🤖'}
       </div>
       <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">
         <MemoizedReactMarkdown
