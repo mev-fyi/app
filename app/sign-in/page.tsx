8c08697cd7137f84c55c0818bff73f4bcb72262c
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-27 20:04:20 +0100

    fix: revert sign-in page to pre-WL changes

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,36 +1,16 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { toast, Toaster } from 'react-hot-toast';
-import { LoginButton } from '@/components/login-button';
+import { auth } from '@/auth'
+import { LoginButton } from '@/components/login-button'
+import { redirect } from 'next/navigation'
 import Image from 'next/image';
 
-export default function SignInPage() {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Fetch session information
-    fetch('/api/check-session')
-      .then(response => response.json())
-      .then(data => {
-        if (data.loggedIn) {
-          router.push('/');
-        } else {
-          setIsLoading(false);
-          if (data.error === 'not_whitelisted') {
-            toast.error("You are not on the whitelist.");
-          }
-        }
-      });
-  }, [router]);
-
-  if (isLoading) {
-    return <div>Loading...</div>; // Or any loading indicator
+export default async function SignInPage() {
+  const session = await auth()
+  // redirect to home if user is already logged in
+  if (session?.user) {
+    redirect('/')
   }
-
   return (
     <div className="flex flex-col h-screen items-center justify-center py-10 sm:pb-50">
-      <Toaster />
       {/* Welcome message */}
       <h1 className="mb-4 text-3xl font-bold text-center">mev.fyi</h1>
       <h1 className="mb-4 text-3xl font-bold text-center">The Maximal Extractable Value (MEV) research chatbot</h1>
@@ -41,7 +21,7 @@ export default function SignInPage() {
       
       {/* Welcome image */}
       <div className="mb-8">
-        <Image src="/android-chrome-512x512.png" alt="MEV Logo" width={512} height={512} />
+        <img src="android-chrome-512x512.png"/>
       </div>
       
       {/* Login buttons */}
