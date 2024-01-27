7b84b48d05ee707538bf23187c45bd9a24299c11
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-27 20:09:00 +0100

    fix: revert auth to pre-WL changes

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth, { type DefaultSession } from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
+import GitHub from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google';
 
 declare module 'next-auth' {
@@ -11,61 +11,29 @@ declare module 'next-auth' {
   }
 }
 
-const WHITELIST = [
-  'alloweduser@gmail.com',
-  'githubUsername',  // Replace with actual GitHub username
-  'vmeylan',
-  'freddmannen',
-  // ... more allowed emails or GitHub usernames
-];
-
 export const {
   handlers: { GET, POST },
   auth,
   CSRF_experimental // will be removed in future
 } = NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || '',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-    }),
+  providers: [GitHub,
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
-  ],
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  })],
   callbacks: {
-    async signIn({ account, profile, email }) {
-      if (!account || !profile) {
-        return false;
-      }
-
-      let identifier = '';
-
-      if (account.provider === 'github' && typeof profile.login === 'string') {
-        identifier = profile.login;
-      } else if (account.provider === 'google' && typeof email === 'string') {
-        identifier = email;
-      }
-
-      if (!WHITELIST.includes(identifier)) {
-        throw new Error('not_whitelisted');
-      }
-
-      return true;
-    },
     jwt({ token, profile }) {
       if (profile) {
-        token.id = profile.id?.toString() || '';
-        token.image = profile.avatar_url || profile.picture || '';
+        token.id = profile.id
+        token.image = profile.avatar_url || profile.picture
       }
-      return token;
+      return token
     },
-    // Reuse any other callbacks from the original configuration
+    authorized({ auth }) {
+      return !!auth?.user // this ensures there is a logged in user for -every- request
+    }
   },
   pages: {
-    signIn: '/sign-in', // Make sure this path is correct.
-    error: '/sign-in',  // Change this path to match your sign-in page path.
-  },
-  // Reuse any other configurations from the original setup
-});
+    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
+  }
+})
