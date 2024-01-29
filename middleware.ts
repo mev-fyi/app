89812f7e54cc4076e4c55880b056079ee04ce891
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-29 19:56:39 +0100

    fix attempt: modify middleware to accommodate specific path

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from './auth'
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [
+    '/((?!api|app/api/create-shared-chat|_next/static|_next/image|favicon.ico).*)'
+  ]
 }
