0b5a68d1225af90a2a59f2e6faf7ae24d350f347
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-30 19:16:12 +0100

    feat: update path because its in /share x2

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ export { auth as middleware } from './auth';
 
 export const config = {
   matcher: [
-    // Exclude specific API routes and the share page route from the middleware
-    '/((?!api/create-shared-chat|_next/static|_next/image|favicon.ico|share/[^/]+/page).*)'
+    // Exclude specific API routes and any route starting with /share/ from the middleware
+    '/((?!api/create-shared-chat|_next/static|_next/image|favicon.ico|share/.*).*)'
   ]
 };
