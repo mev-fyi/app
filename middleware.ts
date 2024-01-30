1c165ba984ad28536a61907453a3afc22585dfa5
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-30 18:41:59 +0100

    feat: do not require auth for shared chat obvs

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-export { auth as middleware } from './auth'
+export { auth as middleware } from './auth';
 
 export const config = {
   matcher: [
-    // Exclude specific API routes from the middleware
-    '/((?!api/create-shared-chat|_next/static|_next/image|favicon.ico).*)'
+    // Exclude specific API routes and the share page route from the middleware
+    '/((?!api/create-shared-chat|_next/static|_next/image|favicon.ico|app/share/[^/]+/page).*)'
   ]
-}
+};
