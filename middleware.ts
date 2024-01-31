c11b5863b64c8968247fcbbb502b026bcb0cf546
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-31 13:00:18 +0100

    fix attempt to login issue: revert middleware, keep api/

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
-export { auth as middleware } from './auth';
+export { auth as middleware } from './auth'
 
 export const config = {
-  matcher: [
-    // Exclude specific API routes and any route starting with /share/ from the middleware
-    '/((?!api/create-shared-chat|_next/static|_next/image|favicon.ico|share/.*).*)'
-  ]
-};
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
