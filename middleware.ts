90ad554f28c74b8171fd2836db9135e8aed565ba
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-08 17:27:06 +0100

    fix attempt shared links: middleware fun

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,6 @@ export { auth as middleware } from './auth';
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)', // Exclude API routes, Next.js static files, and favicon.ico
-    '/share/[id]', // Allow rendering pages under the /share/[id] path without authentication
-    '/app/share/[id]', // Allow rendering pages under the /share/[id] path without authentication
-    '/app/share/.*', // Allow rendering pages under the /share/[id] path without authentication
+    '/((?!api|_next/static|_next/image|favicon.ico|app/share/|/share/[id]).*)', // Exclude API routes, Next.js static files, favicon.ico, and /app/share/ paths
   ],
 };
