ef156f167ea2d8f7b3483526292d90b71b588f0d
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-08 17:17:38 +0100

    fix attempt for chat sharing w/o logging: add app/share/[id] path

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,11 @@
 export { auth as middleware } from './auth';
 
+// https://nextjs.org/docs/pages/building-your-application/routing/middleware
+
 export const config = {
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico).*)', // Exclude API routes, Next.js static files, and favicon.ico
     '/share/[id]', // Allow rendering pages under the /share/[id] path without authentication
+    '/app/share/[id]', // Allow rendering pages under the /share/[id] path without authentication
   ],
 };
