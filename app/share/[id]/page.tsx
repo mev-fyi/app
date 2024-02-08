84a9f765804bf9a9ec5e5f949b5fb379c11b9a92
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-08 16:07:44 +0100

    feat: moar bottomg padding

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -42,7 +42,7 @@ export default async function SharePage({ params }: SharePageProps) {
   // TODO 2024-01-30: add bottom padding to metadata container
   return (
     <>
-      <div className="flex-1 space-y-6 pb-50"> {/* Added bottom padding to the whole container */}
+      <div className="flex-1 space-y-6 pb-300"> {/* Added bottom padding to the whole container */}
         <div className="px-4 py-6 border-b bg-background md:px-6 md:py-8">
           <div className="max-w-2xl mx-auto md:px-6">
             <div className="space-y-1 md:-mx-8">
