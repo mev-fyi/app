b21180f5e979e95597a64f6e72b3a27213a6c9a2
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-08 17:11:08 +0100

    fix: do not redirect to auth for shared pages

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
-export { auth as middleware } from './auth'
+export { auth as middleware } from './auth';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-}
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)', // Exclude API routes, Next.js static files, and favicon.ico
+    '/share/[id]', // Allow rendering pages under the /share/[id] path without authentication
+  ],
+};
