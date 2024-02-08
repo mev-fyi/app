da64bbd6e7e35c27aea6e403dfa12744ca278e65
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-08 17:33:55 +0100

    fix attempt shared links: endless middleware fun.

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ export { auth as middleware } from './auth';
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|app/share/|/share/[id]).*)', // Exclude API routes, Next.js static files, favicon.ico, and /app/share/ paths
+    '/((?!api|_next/static|_next/image|favicon.ico|app/share/|share/).*)', // Exclude API routes, Next.js static files, favicon.ico, and incorrectly specified /app/share/ paths
+    '/share/:path*', // Correctly allow rendering pages under the /share/[id] path without authentication
   ],
 };
