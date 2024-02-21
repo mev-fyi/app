ba1ed8bd872388f4f359969bfae39af8e8e35cee
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-21 12:35:53 +0100

    fix attempt: log in session

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,6 @@ export { auth as middleware } from './auth';
 export const config = {
   matcher: [
     // Exclude specific API routes, the share page route, and the generic sign-in route (including query parameters) from the middleware
-    '/((?!api/create-shared-chat|_next/static|_next/image|favicon.ico|app/share/[^/]+/page|share.*|sign-in).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico|app/share/[^/]+/page|share.*|sign-in).*)'
   ]
 };
