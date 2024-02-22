09de33152107deba873687d16c42142cb25e0559
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-22 18:28:36 +0100

    refactor: try fixing usage of fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={cn(
           'font-sans antialiased',
-          fontSans.variable,
-          fontMono.variable
         )}
       >
         <Toaster />
