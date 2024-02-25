5ddb440064dfdfbfce49f2bbeee126fd8f709784
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-25 12:19:54 +0100

    feat: move share chat icon to the left attempt #4

--- lib/constants.ts
+++ lib/constants.ts
@@ -36,7 +36,7 @@ export const questions = [
     "What is Trusted Execution Environment (TEE)?",
     "What is Multi Party Computation (MPC)?",
     // "How does Trusted Execution Environment (TEE) and Multi Party Computation (MPC) relate to MEV?", // 2023-12-30: does not do well on those
-    "What is Ethereum alignment?",
+    // "What is Ethereum alignment?",  // note longer working as of 2024-02-25
     // "Return a selection of papers and videos that will introduce me to MEV",
     // "What is the role of the Ethereum Foundation?",
     // "How does Flashbots contribute to the MEV space?",
