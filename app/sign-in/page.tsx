5281322219aeff31c5b35e4dbf2ec22ab474298c
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-28 15:38:05 +0100

    feat: Reduce top padding and increase text and container size for better readability

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,28 +10,28 @@ export default async function SignInPage() {
     redirect('/')
   }
   return (
-    <div className="flex flex-col h-screen items-center justify-center py-10 sm:pb-50">
-      {/* Increase the max width class to max-w-2xl or more */}
-      <div className="w-full max-w-2xl px-3">
-        <h1 className="mb-4 text-3xl font-bold text-center">mev.fyi</h1>
-        <h1 className="mb-4 text-3xl font-bold text-center">The Flashbots-grantee Maximal Extractable Value (MEV) research chatbot</h1>
+    <div className="flex flex-col h-screen items-center justify-center py-6 sm:pb-50">
+      {/* Increased the max width class to max-w-4xl for larger text size */}
+      <div className="w-full max-w-4xl px-3">
+        <h1 className="mb-4 text-4xl font-bold text-center">mev.fyi</h1>
+        <h1 className="mb-4 text-4xl font-bold text-center">The Flashbots-grantee Maximal Extractable Value (MEV) research chatbot</h1>
         
-        <p className="mb-6 leading-normal text-muted-foreground">
+        <p className="mb-6 leading-normal text-muted-foreground text-lg">
           Find the latest MEV-related research, across mechanism design, auctions, information privacy, from docs, research papers, articles and YouTube videos.
         </p>
         
-        {/* Empty paragraph for spacing */}
-        <p className="my-6"></p>
+        {/* Reduced vertical spacing */}
+        <p className="my-4"></p>
         
-        <p className="leading-normal text-muted-foreground">
+        <p className="leading-normal text-muted-foreground text-lg">
           To keep our platform friendly and secure, we use GitHub or Google login solely as a spam prevention measure. 
           We promise not to collect any data from you. Once you&apos;re logged in, you&apos;re all set to explore without any hassle.
           Thanks for helping us keep the community safe and sound!
         </p>
       </div>
       
-      {/* Empty paragraph for spacing */}
-      <p className="my-6"></p>
+      {/* Reduced vertical spacing */}
+      <p className="my-4"></p>
       
       <div>
         <LoginButton loginType="github" text="Login with GitHub" showIcon />
