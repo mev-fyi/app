403ec79f95b1d6455745dfd9bbabced1fb2c2f1c
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-28 15:22:26 +0100

    feat: line returns welcome page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -11,22 +11,27 @@ export default async function SignInPage() {
   }
   return (
     <div className="flex flex-col h-screen items-center justify-center py-10 sm:pb-50">
-      {/* Container for the title and text, limiting its width */}
       <div className="w-full max-w-xl px-10">
-        {/* Welcome message */}
         <h1 className="mb-4 text-3xl font-bold text-center">mev.fyi</h1>
         <h1 className="mb-4 text-3xl font-bold text-center">The Flashbots-grantee Maximal Extractable Value (MEV) research chatbot</h1>
-        {/* Text block */}
+        
         <p className="mb-6 leading-normal text-muted-foreground">
           Find the latest MEV-related research, across mechanism design, auctions, information privacy, from docs, research papers, articles and YouTube videos.
         </p>
+        
+        {/* Empty paragraph for spacing */}
+        <p className="my-6"></p>
+        
         <p className="leading-normal text-muted-foreground">
           To keep our platform friendly and secure, we use GitHub or Google login solely as a spam prevention measure. 
           We promise not to collect any data from you. Once you&apos;re logged in, you&apos;re all set to explore without any hassle.
           Thanks for helping us keep the community safe and sound!
         </p>
       </div>
-      {/* Login buttons */}
+
+      {/* Empty paragraph for spacing */}
+      <p className="my-6"></p>
+      
       <div>
         <LoginButton loginType="github" text="Login with GitHub" showIcon />
         <LoginButton loginType="google" text="Login with Google" showIcon />
