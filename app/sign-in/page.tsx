f6495271c1ff95f7715db027073f3cec186470ed
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-29 15:54:57 +0100

    feat: resize container as we resize the window for sign-in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,27 +10,29 @@ export default async function SignInPage() {
     redirect('/')
   }
   return (
-    <div className="flex flex-col h-screen justify-start pt-20 sm:pt-32">
-      <div className="w-full max-w-8xl px-3 mx-auto text-center">
+    <div className="flex flex-col h-screen justify-start items-center pt-20 sm:pt-32">
+      <div className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl px-3 mx-auto text-center">
         <h1 className="mb-4 text-4xl font-bold">mev.fyi</h1>
-        <h1 className="mb-4 text-4xl font-bold">The Flashbots-grantee Maximal Extractable Value (MEV) research chatbot</h1>
+        <h1 className="mb-4 text-2xl sm:text-3xl md:text-4xl font-bold">
+          The Flashbots-grantee Maximal Extractable Value (MEV) research chatbot
+        </h1>
         
-        <p className="mb-6 leading-normal text-muted-foreground text-lg">
+        <p className="mb-6 text-base sm:text-lg leading-normal text-muted-foreground">
           Find the latest MEV-related research, across mechanism design, auctions, information privacy, from docs, research papers, articles and YouTube videos.
         </p>
         
         <p className="my-4"></p>
         
-        <p className="leading-normal text-muted-foreground text-lg">
+        <p className="text-base sm:text-lg leading-normal text-muted-foreground">
           To keep our platform friendly and secure, we use GitHub or Google login solely as a spam prevention measure. 
           We promise not to collect any data from you! 
         </p>
 
-        <p className="leading-normal text-muted-foreground text-lg">
+        <p className="text-base sm:text-lg leading-normal text-muted-foreground">
           Once you&apos;re logged in, you&apos;re all set to explore without any hassle.
         </p>  
 
-        <p className="leading-normal text-muted-foreground text-lg">
+        <p className="text-base sm:text-lg leading-normal text-muted-foreground">
           Thanks for helping us keep the community safe and sound!
         </p>
       </div>
